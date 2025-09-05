ea9063eb457266e54520979a885f0cb2233de5f1
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-09-05 22:46:14 +0700

    change logic to multi database

--- src/config/db/connection.js
+++ src/config/db/connection.js
@@ -9,7 +9,8 @@ if (process.env.DB_DIALECT === "sqlite") {
   // Config SQLite
   sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: process.env.DB_STORAGE || "database.sqlite", 
+    storage: process.env.DB_STORAGE || "database.sqlite",
+    logging: process.env.NODE_ENV === "development" ? console.log : false,
   });
 } else {
   // Config MySQL/Postgres/MariaDB/Tedious (Microsoft SQL Server)/OracleDB (Oracle Database)
@@ -21,9 +22,15 @@ if (process.env.DB_DIALECT === "sqlite") {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       dialect: process.env.DB_DIALECT,
-      dialectOptions: {
-        connectTimeout: 60000,
+      logging: process.env.NODE_ENV === "development" ? console.log : false,
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
       },
+      dialectOptions:
+        process.env.DB_DIALECT === "mysql" ? { connectTimeout: 60000 } : {},
     }
   );
 }
@@ -32,7 +39,7 @@ const testConnection = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    console.log(`Running in ${process.env.NODE_ENV} mode`); 
+    console.log(`Running in ${process.env.NODE_ENV} mode`);
   } catch (error) {
     console.error("Unable to connect to the database:", error.message);
   }
