6b39a3cfd18a4cbe77cbf438c4d610a56beef1f7
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-09-10 11:26:36 +0700

    grouping routes

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const express = require('express');
-const { sequelize, testConnection } = require('./src/config/db/connection');
-const authRoutes = require("./src/routes/authRoutes");
-const categoryRoutes = require("./src/routes/categoryRoutes");
-const productRoutes = require("./src/routes/productRoutes");
-const variantRoutes = require("./src/routes/variantRoutes");
-const cartRoutes = require("./src/routes/cartRoutes");
-const orderRoutes = require("./src/routes/orderRoutes");
-const paymentMethodRoutes = require("./src/routes/paymentMethod");
+const express = require("express");
+const { testConnection } = require("./src/config/db/connection");
+const routes = require("./src/routes/index.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/variants", variantRoutes);
-app.use("/api/carts", cartRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/payment-methods", paymentMethodRoutes);
+app.use("/api", routes);
 
 const startServer = async () => {
   await testConnection();
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const authRoutes = require("../routes/authRoutes");
+const categoryRoutes = require("../routes/categoryRoutes");
+const productRoutes = require("../routes/productRoutes");
+const variantRoutes = require("../routes/variantRoutes");
+const cartRoutes = require("../routes/cartRoutes");
+const orderRoutes = require("../routes/orderRoutes");
+const paymentMethodRoutes = require("../routes/paymentMethod");
+
+const router = express.Router();
+router.use(express.json());
+
+router.use("/auth", authRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/variants", variantRoutes);
+router.use("/carts", cartRoutes);
+router.use("/orders", orderRoutes);
+router.use("/payment-methods", paymentMethodRoutes);
+
+module.exports = router;
