9d847a998f48ef41f660f515d5ac5fa9a3b3cfee
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-09-12 12:45:33 +0700

    add cors in server

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { testConnection } = require("../config/db/connection");
 const routes = require("../routes/index.js");
 const { notFoundResponse } = require("../utils/responses");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
